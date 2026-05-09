4802e21885bcca90b7135e625f9ef5c59b809f47
radityakrisdandi <radityakrisdandi31@gmail.com>
2026-05-09 19:53:42 +0700

    Raditya Krisdandi Yudistira / 2402310088 : Menambahkan migration

---
+++ database/migrations/2026_05_09_122028_create_beritas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('berita', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('admin_id')
+                ->constrained('users')
+                ->onDelete('cascade');
+
+            $table->string('judul');
+            $table->text('isi_berita');
+            $table->string('gambar')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('beritas');
+    }
+};
---
+++ database/seeders/LogAktivitasSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class LogAktivitasSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
